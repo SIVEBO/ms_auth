6057e067e49c5821b1e9cc3996afa55608ea1ce4
daemunozr <dae.munozr@duocuc.cl>
2026-06-20 12:37:40 -0400

    eliminado microservicio duplicado dentro de src

--- src/main/java/com/sivebo/ms_admision/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sivebo.ms_admision.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/sivebo/ms_admision/exception/ExternalServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.sivebo.ms_admision.exception;
-
-public class ExternalServiceException extends RuntimeException {
-    public ExternalServiceException(String message) {
-        super(message);
-    }
-}
