7eb83a212b86ce4b2f4c40cbf57f7f5f065ed582
Lukas Avila <137963150+LukasAvilaPonce@users.noreply.github.com>
2026-07-11 10:25:47 -0400

    permitir recursos swagger

--- src/main/java/com/sivebo/ms_auth/config/SecurityConfig.java
+++ src/main/java/com/sivebo/ms_auth/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
-                                .requestMatchers("/doc/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                                .requestMatchers("/doc/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**").permitAll()
                                 .requestMatchers("/actuator/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/usuarios/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").hasRole("ADMIN")
